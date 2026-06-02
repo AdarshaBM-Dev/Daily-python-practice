27070313e024ddee5d6a0309b68a69e52ef7e889
Adarsha B M <adarsha23961@gmail.com>
2026-06-02 13:07:59 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,41 @@
+a = 10
+b = 22
+c = 20
+
+print(a)
+print(a+b)
+
+x, y, z = 10, 20, 30
+x = y = z = 10
+
+#case sensetive
+name = "Krishna"
+Name = "Adarsha"
+print(name)
+
+#data types
+name = "Adarsha" #string
+age = 22 #int
+is_student = False #bool
+weight = 70.5 #floot #decimal
+
+print(type(weight))
+
+age = float(age)  #type conversion
+print(age)
+
+#operators
+
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) #floor division
+print(a%b)#modulus
+print(a**b)#exponetiation  (SQUARE OF)
+
+x, y = 10, 50
+x, y = y, x
+print("x:", x) # 50
+print("y:", y) # 10
